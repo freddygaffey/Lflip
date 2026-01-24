235ded46a7252610f7400e3d851e090609ce8442
Fred Gaffey <fredgaffey08@gmail.com>
2026-01-24 14:29:21 +1100

    now can scan for networks

--- esp_server1/esp_server1.ino
+++ esp_server1/esp_server1.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <NetworkClient.h>
 #include <WiFiAP.h>
+#include <vector>
 
 
 NetworkServer server(80);
@@ -25,9 +26,28 @@ bool start_ap(){
   return true;
 }
 
+std::vector<String> get_ssid_scan() {
+  WiFi.STA.begin();
+  // WiFi.scanNetworks will return the number of networks found.
+  int n = WiFi.scanNetworks();
+  std::vector<String> networks;
+  for (int i = 0; i < n; ++i) {
+    networks.push_back( WiFi.SSID(i).c_str());
+    // Serial.printf(WiFi.SSID(i).c_str());
+    // Serial.printf("\n");
+  }
+  return networks;
+  
+}
 void setup() {
   Serial.begin(115200);
   start_ap();
+  get_ssid_scan();
+
+  // std::vector<String> networks = get_ssid();
+  // for (int i = 0; i < networks.size(); ++i) {
+  //   Serial.println(networks[i]);
+  // }
 }
 
 
