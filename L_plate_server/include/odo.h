c3c93e9b1dccda01df50955d1f84a403fa5d36a8
Fred Gaffey <fredgaffey08@gmail.com>
2026-01-29 12:54:31 +1100

    fix main at repo on git hub init

--- L_plate_server/include/odo.h
+++
@@ -1,9 +0,0 @@
-#include <Preferences.h>
-#include <Arduino.h>
-
-// #include "logging.h"
-
-void init_odo();
-unsigned long get_odo();
-void set_odo(unsigned long odo_new);
-unsigned long update_ODO_with_speed(float speed_mps);
