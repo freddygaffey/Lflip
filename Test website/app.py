85530ddd3099afc55b22ca76afbb071c9029acae
Fred Gaffey <fredgaffey08@gmail.com>
2026-01-30 15:16:17 +1100

    now can send start and stop commands to the esp 32

--- Test website/app.py
+++ Test website/app.py
@@ -1,15 +1,26 @@
+
 # config.py
 ESP_IP = '192.168.4.1'
 
-from flask import Flask
+from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def esp_link():
-    return '''
-    <a href="http://192.168.4.1/">Open L-Plate Device</a>
-    <p style="color: gray; font-size: 12px;">
-        (Must be connected to same WiFi as your L-Plate)
-    </p>
-    '''
+def test_controls():
+    current_time = int(time.time())
+    esp_ip = '192.168.4.1'
+    sd_id = 'max_smith'
+    
+    start_url = f"http://{esp_ip}/start?start_time={current_time}&SD_ID={sd_id}"
+    stop_url = f"http://{esp_ip}/stop?end_time={current_time}"
+    odo_url = f"http://{esp_ip}/odo_update?odo=100"
+    sync_url = f"http://{esp_ip}/sync"
+    
+    return render_template('test_controls.html',
+                          start_url=start_url,
+                          stop_url=stop_url,
+                          odo_url=odo_url,
+                          sync_url=sync_url)
