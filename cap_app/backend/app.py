41cd2a25d51aeef86f0831a02506f5d1b05cabeb
Fred Gaffey <fredgaffey08@gmail.com>
2026-05-20 15:36:01 +1000

    update to enable production

--- cap_app/backend/app.py
+++ cap_app/backend/app.py
@@ -407,4 +407,7 @@ def delete_car(car_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True,
+            origins=["lflip.pebnum.com",
+                     "capacitor://localhost",
+                     "http://localhost"])
