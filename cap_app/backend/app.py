0b2f25b91fcf140da1212ae759be6983c8f2641d
Fred Gaffey <fredgaffey08@gmail.com>
2026-05-20 15:56:43 +1000

    add comment

--- cap_app/backend/app.py
+++ cap_app/backend/app.py
@@ -407,7 +407,7 @@ def delete_car(car_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="127.0.0.1", port=5001, debug=True,
+    app.run(host="127.0.0.1", port=5001, debug=True, # TODO: PROD: remove befor producion
             origins=["lflip.pebnum.com",
                      "capacitor://localhost",
                      "http://localhost"])
